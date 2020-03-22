53f4d4f9c839a3c0989432d2487a23d83f8f50ab
unknown <ufetz@student.kit.edu>
2020-03-22 11:22:56 +0100

    working implementation of GrabCut with mask

--- src/SegPandaGrabCut.py
+++ src/SegPandaGrabCut.py
@@ -3,37 +3,32 @@ import numpy as np
 
 # Load image
 img_bgr = cv.imread('.\images\panda.jpg')
-newmask = cv.imread('.\images\panda_mask.jpg',0)
+newmask = cv.imread('.\images\panda_mask3.jpg',0)
 
 [height,width,channels] = img_bgr.shape
-#print(height,width,channels)
 
-# Convert to HSV
-img_hsv = cv.cvtColor(img_bgr, cv.COLOR_BGR2HSV)
-
-# Separate channels
-img_h = img_hsv[:,:,0]
-img_s = img_hsv[:,:,1]
-img_v = img_hsv[:,:,2]
-
+# Set parameter for grabCut function
 mask = np.zeros((height,width),np.uint8)
+mask[25:298, 140:368] = 3
 mask[newmask==0] = 0
 mask[newmask==255] = 1
-
+rect = (140,25,228,273)
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
-cv.grabCut(img_bgr, mask, None, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_MASK)
 
-mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+cv.grabCut(img_bgr, mask, rect, bgdModel, fgdModel, 1, cv.GC_INIT_WITH_MASK)
+#cv.grabCut(img_bgr, mask, rect, bgdModel, fgdModel, 1, cv.GC_INIT_WITH_RECT)
 
-mask3 = np.where((mask==2)|(mask==0),0,255).astype('uint8')
-cv.imshow('mask3',mask3)
-
-img_seg = img_bgr*mask2[:,:,np.newaxis]
+mask2 = np.where((mask==2)|(mask==0),0,255).astype('uint8')
+img_seg = cv.bitwise_and(img_bgr, img_bgr, mask=mask2)
 
 # Display images
 cv.imshow('original',img_bgr)
+cv.imshow('newmask',newmask)
+cv.imshow('mask',mask2)
 cv.imshow('image segmentated',img_seg)
+
+cv.imwrite('.\images\pandaSegGrabCut1.jpg',img_seg)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
